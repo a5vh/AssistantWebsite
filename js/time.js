d47f5570bf48637face993df053759f311713d1f
August H. Hummert V <august.hummert5@gmail.com>
2019-04-24 13:28:34 -0500

    got libraries
    weeeeeeeeeeeeeeeeeeeeeee

---
+++ js/time.js
@@ -0,0 +1,2 @@
+jQuery.getJSON("http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=redmond%2Cwa&wp.1=Issaquah%2Cwa&avoid=minimizeTolls&key=AutE4ZIQ-eedhR1pRQH9I-t3vEKDDVvt9QxBK1qg8XOPoTq2n2AdC0xmrFuqofHe");
+
